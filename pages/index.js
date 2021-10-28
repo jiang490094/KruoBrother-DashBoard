af6cb815dc2064eb94efff30bee68e7b79cf5e8f
ericfei <eric.fei@kuobrothers.com>
2021-10-28 17:51:45 +0800

    update: update set up

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function HomePage() {
-  return <div>Welcome to 3413214!</div>;
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+function HomePage({ className }) {
+  return <div className={className}>Welcome to alpha!</div>;
 }
 
-export default HomePage;
+export default styled(HomePage)`
+  color: red;
+`;
+
+HomePage.propTypes = {
+  className: PropTypes.string.isRequired
+};
