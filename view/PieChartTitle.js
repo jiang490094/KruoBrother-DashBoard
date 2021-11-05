f255d9051bc9962cf87683816e54a427c140bf8a
jiang490094 <jfr08100810@gmail.com>
2021-11-05 14:54:21 +0800

    faet Marquee

--- view/PieChartTitle.js
+++ view/PieChartTitle.js
@@ -1,7 +1,25 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { useContext } from "react";
+import Marquee from "react-fast-marquee";
 
-const PieChartTitle = ({ className, siteName }) => {
+import { GlobalContext } from "../provider/globalprovider";
+
+const PieChartTitle = ({ className, siteName, siteColor }) => {
+  const { rankDisplay } = useContext(GlobalContext);
+  let topArray = [];
+  let site = "buy123";
+
+  if (siteName === "松果購物") {
+    site = "pcone";
+  }
+  // console.log(rankDisplay);
+  for (let i = 0; i < rankDisplay.length; i++) {
+    if (rankDisplay[i].site == site) {
+      topArray.push(rankDisplay[i].item_name);
+    }
+  }
+  // console.log(topArray[0]);
   return (
     <>
       <div className={className}>
@@ -10,11 +28,23 @@ const PieChartTitle = ({ className, siteName }) => {
           <div className="previous-word">
             <span className="tiny-words">集團</span>
             <span className="title-words">{siteName}</span>
-            <span>
+            {/* <span>
               <p className="category-words">分類排行</p>
               <p className="eight-words">前8名熱門排行</p>
-            </span>
+            </span> */}
           </div>
+        </div>
+
+        <div className="number">
+          <p className="eight-words">top product</p>
+          <p
+            className="title-words number-font"
+            style={{ color: `${siteColor}` }}
+          >
+            <Marquee gradient={false} gradientWidth="50" speed="50">
+              {topArray[0]}
+            </Marquee>
+          </p>
         </div>
 
         <img className="under-line" src="/Images/pie-under-line.png" />
@@ -26,6 +56,7 @@ const PieChartTitle = ({ className, siteName }) => {
 PieChartTitle.propTypes = {
   className: PropTypes.string.isRequired,
   siteName: PropTypes.string.isRequired,
+  siteColor: PropTypes.string.isRequired,
   number: PropTypes.number.isRequired
 };
 
@@ -66,13 +97,30 @@ export default styled(PieChartTitle)`
     color: white;
   }
 
+  .number {
+    position: absolute;
+    top: 10px;
+    right: 0;
+    max-width: 200px;
+  }
   .eight-words {
     font-size: 12px;
     color: rgba(255, 255, 255, 0.6);
   }
 
+  .number-font {
+    font-size: 20px;
+    font-family: Prohibition;
+    line-height: 40px;
+    color: rgba(255, 107, 0, 1);
+  }
+
   .under-line {
     display: block;
     width: 100%;
+  }
+
+  .marquee {
+    margin-right: 50px;
   }
 `;
