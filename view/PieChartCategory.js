06d9a0dadfaa0f970692522fbbd11c81fbba94a5
jiang490094 <jfr08100810@gmail.com>
2021-11-05 11:28:31 +0800

    fix color

--- view/PieChartCategory.js
+++ view/PieChartCategory.js
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { useContext } from "react";
-import CountUp from "react-countup";
 
 import { GlobalContext } from "../provider/globalprovider";
 
@@ -24,17 +23,29 @@ const PieChartCategory = ({ className, siteName }) => {
   // console.log("pconeSum", pconeSum);
   // console.log("buy123Sum", buy123Sum);
 
+  // const colors = [
+  //   "rgb(255,111,0,0.7)",
+  //   "rgb(255,143,0,0.7)",
+  //   "rgb(255,160,0,0.7)",
+  //   "rgb(255,179,0,0.7)",
+  //   "rgb(255,193,7,0.7)",
+  //   "rgb(255,202,40,0.7)",
+  //   "rgb(255,213,79,0.7)",
+  //   "rgb(255,224,130,0.7)",
+  //   "rgb(255,236,179,0.7)",
+  //   "rgb(255,248,225,0.7)"
+  // ];
   const colors = [
-    "rgba(24, 254, 254, 0.7)",
-    "rgba(255, 153, 0, 0.7)",
-    "rgba(151, 0, 175, 0.7)",
-    "rgba(255, 92, 22, 0.7)",
-    "rgba(255, 0, 214, 0.7)",
-    "rgba(255, 61, 0, 0.7)",
-    "rgba(82, 0, 255, 0.7)",
-    "rgba(6, 132, 248, 0.7)",
-    "rgba(149, 132, 255, 0.7)",
-    "rgba(5, 0, 232, 0.7)"
+    "rgba(24, 254, 254, 0.65)",
+    "rgba(255, 153, 0, 0.65)",
+    "rgba(151, 0, 175, 0.65)",
+    "rgba(255, 92, 22, 0.65)",
+    "rgba(255, 0, 214, 0.65)",
+    "rgba(255, 61, 0, 0.65)",
+    "rgba(82, 0, 255, 0.65)",
+    "rgba(6, 132, 248, 0.65)",
+    "rgba(149, 132, 255, 0.65)",
+    "rgba(5, 0, 232, 0.65)"
   ];
 
   return (
@@ -42,14 +53,14 @@ const PieChartCategory = ({ className, siteName }) => {
       <div>
         {obj.map((i, index) => {
           if (index % 2 == 0) {
-            let percent = (i.amount / objtotal) * 100;
+            let percent = Math.round((i.amount / objtotal) * 100);
             return (
               <div className="category-list" key={index}>
                 <div
                   className="category-number"
                   style={{ borderLeft: `4px solid ${colors[index]}` }}
                 >
-                  <CountUp start={0} end={percent} duration={2} />%
+                  {percent}%
                 </div>
                 <div className="category-name">{i.category_name}</div>
               </div>
@@ -68,14 +79,14 @@ const PieChartCategory = ({ className, siteName }) => {
       <div>
         {obj.map((i, index) => {
           if (index % 2 != 0) {
-            let percent = (i.amount / objtotal) * 100;
+            let percent = Math.round((i.amount / objtotal) * 100);
             return (
               <div className="category-list" key={index}>
                 <div
                   className="category-number"
                   style={{ borderLeft: `4px solid ${colors[index]}` }}
                 >
-                  <CountUp start={0} end={percent} duration={2} />%
+                  {percent}%
                 </div>
                 <div className="category-name">{i.category_name}</div>
               </div>
